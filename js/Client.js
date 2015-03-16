d1ba598efb462d6edc91993f20f0d6c3d386e7bd
Adam Bergen <bergen@gmail.com>
2015-03-15 23:39:32 -0500

    portfolio project

---
+++ js/Client.js
@@ -0,0 +1,39 @@
+;(function(m){
+
+	$(".clicker").on("click", function(){
+		$(".menu-collapsed").toggle()
+		$(".menu-expanded").toggleClass('active');
+	});
+
+	$(".exit").on("click", function(){
+		$(".menu-collapsed").toggle()
+		$(".menu-expanded").toggleClass('active');
+	});
+
+	$('a[href*=#]:not([href=#])').on('click',function() {
+
+        //location.pathname takes everything after the hostname
+        if ( location.hostname == this.hostname 
+            ) {
+            
+            console.log('current-y-offset: '+$(this).offset().top)
+
+            //capture the hash-tag in the 
+            var target = $('[name=' + this.hash.slice(1) + ']');
+            
+            console.log('target-y-offset: '+target.offset().top)
+            //$(el).length > 1 determines that there is an element in the DOM
+            if (target.length) {
+                $('html,body').animate({
+                    //animate from the current position
+                    //   and set the target element's top y-offset as the scrollTop position to the 
+                    //      $('a[name='contact']). «return positionOfEl». «'top' property»
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+
+
+})(typeof module === "object" ? module.exports : window);
